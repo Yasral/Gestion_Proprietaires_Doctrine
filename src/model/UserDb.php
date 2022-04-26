017b6e55168bb4cbbc876b6a38c1d3c84e96ba28
Jacques Etienne <santiagondiaye1@gmail.com>
2022-04-26 13:50:48 +0000

    Creation des methodes dans le model User

---
+++ src/model/UserDb.php
@@ -0,0 +1,21 @@
+<?php 
+
+    namespace src\model;
+    use libs\system\Model;
+
+    class UserDb extends Model{
+
+        public function registerUser($administrator){
+
+        }
+
+        public function login($username, $password){
+
+        }
+
+        public function findUserByEmail($email){
+
+        }
+    }
+
+?>
