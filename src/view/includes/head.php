eab4383de903d1c449181625b4b324013c8b72bd
Jacques Etienne <santiagondiaye1@gmail.com>
2022-04-26 13:27:55 +0000

    Creation du dossier includes qui va contenir le header, le footer et les scripts pour eviter la repetition

---
+++ src/view/includes/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="HTML and CSS form">
+    <meta name="name" content="Yasral">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo URLROOT ?>/assets/images/favicon/favicon-32x32.png">
+
+    <!-- Have to look about how to integrate the style.css and the app.js file -->
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/assets/css/style.css">
+
+    <!-- <link rel="stylesheet" href="style.css"> -->
+
+    <script src="<?php echo URLROOT ?>/assets/js/app.js" defer="true"></script>
+    <!-- Have to look about how to integrate the style.css and the app.js file -->
+    <title>Dashboard</title>
+</head>
+<body>
+
