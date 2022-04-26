78fdf46bf32039e0f27967d8d5dc49c4d119502d
Jacques Etienne <santiagondiaye1@gmail.com>
2022-04-26 13:53:45 +0000

    Creation des methodes dans le model Proprietaire

---
+++ src/model/ProprietaireDb.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace src\model;
+use libs\system\Model;
+
+class ProprietaireDb extends Model{
+
+    public function add($proprietaire){
+
+    }
+
+    public function findAll(){
+
+    }
+
+    public function deleteRecord($id){
+
+    }
+
+    public function editRecord($id){
+
+    }
+
+    public function editRecordBis(){
+        
+    }
+}
+
+?>
