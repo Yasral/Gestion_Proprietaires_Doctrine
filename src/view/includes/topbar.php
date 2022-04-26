84b9fcb2322fcf6ce12532f9b501e61bacc4d544
Jacques Etienne <santiagondiaye1@gmail.com>
2022-04-26 13:30:54 +0000

    Creation et inclusion de la barre de recherche dans le dossier includes

---
+++ src/view/includes/topbar.php
@@ -0,0 +1,13 @@
+ <!-- Topbar menu -->
+ <div class="topbar">
+    <div class="toogle">
+        <ion-icon name="menu-outline"></ion-icon>
+    </div>
+            
+    <div class="search">
+        <label for="">
+            <input type="text" name="" id="" placeholder="Rechercher">
+            <ion-icon name="search-outline"></ion-icon>
+        </label>
+    </div>
+</div>
